dd88be7264ef4564f423d3b83fe9e3592c21322f
Rajiv-Sharma23 <rajivsharma0246@gmail.com>
2025-02-08 13:43:06 +0530

    learning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
 
-const page = () => {
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { signIn } from "./actions/sign-in";
+import { signOut } from "./actions/sign-out";
+import { auth } from "@/auth";
+
+export default async function Home() {
+  const session = await auth();
+ 
   return (
-    <div>page</div>
-  )
+    <div>
+      <h1>Home</h1>
+      <form action={signIn}>
+        <Button type="submit">Signin</Button>
+      </form>
+      <form action={signOut}>
+        <Button type="submit">Signout</Button>
+      </form>
+      {
+        session?.user && <div>{JSON.stringify(session.user)}</div>
+      }
+    </div>
+  );
 }
 
-export default page
